31498fc344322a70662e3431f690c04776569f3f
rravada <roshan.ravada@gmail.com>
2023-06-21 13:11:28 -0700

    lab2.2

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def main():
     workhours = int(input('Enter your work hours: '))
     reg_hours = 40
-    reg_rate = 18.25
-    ov_rate = 27.78
-
+    ov_hours = workhours - reg_hours
+    reg_rate = 18.25 * workhours
+    ov_rate = 0
+    if ov_hours > 0:
+        ov_rate = 27.78 * ov_hours
+    total_wage= reg_rate+ov_rate
    ##################################################
    # Code your program here
    ##################################################
@@ -12,14 +15,14 @@ def main():
     # regular_wage = reg_hours * reg_rate
     # total_wage = regular_wage + overtime_wage
 
-    print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage}")
-    print(f"Overtime hours: {overtime} Overtime Charge: {overtime_wage:.2f}")
+    print(f"Regular hours: {reg_hours} Regular Charge: {reg_rate}")
+    print(f"Overtime hours: {ov_hours} Overtime Charge: {ov_rate:.2f}")
     print(f"Total wage : {total_wage:.2f}")
 
    ##################################################
    # Do not delete the return statement
    ##################################################
-    return regular_wage, overtime_wage, total_wage
+    return reg_rate , ov_rate , total_wage
 
 
 if __name__ == '__main__':
